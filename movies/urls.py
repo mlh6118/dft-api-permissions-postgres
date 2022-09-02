6dad2c4663cbcbba7ea665f5d632acdbbb99544b
Marni Hager <mlh6118@gmail.com>
2022-09-01 22:47:33 -0700

    uploading modified files.

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,5 @@ from .views import MovieList, MovieDetail
 
 urlpatterns = [
     path('', MovieList.as_view(), name='movie_list'),
-    path('<int:pk>', MovieDetail.as_view(), name='movie_detail')
+    path('/<int:pk>', MovieDetail.as_view(), name='movie_detail')
 ]
